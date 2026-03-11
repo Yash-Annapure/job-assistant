0519c5f627884aa067d461fe886599d4cba997b6
Yash-Annapure <annapureyash@gmail.com>
2026-03-11 15:49:05 +0100

    Docker + PostgreSQL + FastAPI running together Environment variables properly injected SQLAlchemy connected to PostgreSQL All 4 database tables created on startup

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from db.database import engine, Base
 from db import models
+import os
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+
+def create_db_and_tables():
+    Base.metadata.create_all(bind=engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables() #startup
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/health")
 async def health_check():
     return {"status":"ok"}
+
+
+'''
+ Depreciated method
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()   
+'''
+
+
+
+
+
 

bfa4b8384a17db9a5ddef5b7e9a4ddae5ce7047b
Yash-Annapure <annapureyash@gmail.com>
2026-03-11 11:07:28 +0100

    Created databse file with engine instance,base class with metadata to keep a consistent format in the Sub-classes down the line

---
+++ backend/db/database.py
@@ -0,0 +1,31 @@
+#SqlAlchemy is the ORM(Object-Relational-Mapping) which handels the connection between the database 
+from sqlalchemy import create_engine                  
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/postgres_db")
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+#this will serve as the parent class 
+class Base(DeclarativeBase):                                 
+    metadata = MetaData(
+        naming_convention ={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s"
+        }
+    )
+
+
+    
+

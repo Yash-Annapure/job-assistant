6b38e5e4717862fe6e19e39c0f045a53042d30b8
Yash-Annapure <annapureyash@gmail.com>
2026-03-12 11:25:44 +0100

    added get_current_user dependency for protected routes

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -4,10 +4,14 @@ verify_password()      → compares plain text against stored hash
 create_access_token()  → builds a signed JWT with user_id + expiry
 decode_access_token()  → validates and unpacks a JWT
 '''
+from db.database import get_db
+from db.models import User
 import bcrypt
 from datetime import datetime,timedelta
 from jose import JWTError, jwt
 from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -48,5 +52,15 @@ def decode_access_token(token:str):
         
     except jwt.JWTError:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+def get_current_user(token: str = Depends(oauth2_schema), db = Depends(get_db)):
+    get_mail = decode_access_token(token) #already returns an email string
+    existing_mail = db.query(User).filter(User.email == get_mail).first()
+    if not existing_mail:
+        raise HTTPException(401, "Invalid Credentials")
+    return existing_mail
+
+
 
 

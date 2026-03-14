2d0e0154b188615441ee7108da53b649f9a8773c
Yash-Annapure <annapureyash@gmail.com>
2026-03-14 14:01:29 +0100

    AI service layer files

---
+++ backend/ml/llm_service.py
@@ -0,0 +1,4 @@
+class LLMService:
+    def __init__():                #configure gemini with API key
+        async def generator(prompt:str) -> str:       #send prompt, return response
+            pass

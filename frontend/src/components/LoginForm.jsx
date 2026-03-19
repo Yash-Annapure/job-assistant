30d58e29cbf20e0917344c28936f85b471318d02
Yash-Annapure <annapureyash@gmail.com>
2026-03-19 14:49:04 +0100

    login form with persistent auth

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useEffect } from "react"
-
+import { loginUser } from "../api.js"
 function Button({ label, onClick}) {
         return <button onClick={onClick}>{label}</button>
     }
@@ -15,31 +15,20 @@ function LoginForm() {
     }, [])
     
     const handelLogin = async () => {
-        const reponse = await fetch("http://localhost:8000/auth/login",{
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email: email, password: password})
-        })
-        const data = await reponse.json()
-        console.log(data)
-        console.log(data["access_token"])
-        console.log("token value:", data["access_token"])
+        const data = await loginUser(email,password)  // refactored to login user to fetch from servide API layer
         if (data["access_token"]) {
         localStorage.setItem("token", data.access_token)
         setIsLoggedIn(true)
         }
     }
     return (
-        <div>
-            <input onChange={(e) => setEmail(e.target.value)}/>
-            <p>{email}</p>
-            <Button label="Submit" onClick={handelLogin} /> 
-            <input onChange={(e) => setPassword(e.target.value)}/>   
-            <p>{password}</p>
-            <Button label="Submit" onClick={handelLogin} />
-            {isLoggedIn ? <h1> WELCOME!</h1> : <p>Please login</p>}
-        </div>
-    )
+    <div>
+        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+        <Button label="Login" onClick={handelLogin} />
+        {isLoggedIn ? <h1>WELCOME!</h1> : <p>Please login</p>}
+    </div>
+) 
     
 }
 
